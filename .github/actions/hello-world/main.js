eedb8a02f40e6bf382cd600555e4b57e3a62e321
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-01-27 00:13:52 +0100

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,12 @@
-console.log("hello learning lab");
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+
+if (thirdGreeting) {
+  console.log(`Hello ${thirdGreeting}`);
+}
